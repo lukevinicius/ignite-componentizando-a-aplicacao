eabe618a6c5dbdfdce5e9a2d0900562fd7acab82
lucas vinicius <lucasviniciusaalves@gmail.com>
2022-04-07 10:22:17 -0300

    update: Finish challenge

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,38 @@
-export function SideBar() {
+import { useCallback } from "react";
+import { Button } from "./Button";
+
+interface SideBarProps {
+  genres: Array<{
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+  }>
+  selectedGenreId: number;
+  setSelectedGenreId: (id: number) => void;
+}
+
+export function SideBar({ genres, selectedGenreId, setSelectedGenreId }: SideBarProps) {
   // Complete aqui
+  const handleClickButton = useCallback((id: number) => {
+    setSelectedGenreId(id);
+  }, [selectedGenreId])
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
